bb51040f1e671c8b266d565693b91296f2d2e1ba
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 18:49:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,8 +62,8 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql,name).collect do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql,name).collect do |student|
+      self.new_from_db(student)
     end.first
   end
 
